014990b447ca0c3ad178cb78536f43f6ae8974fb
rbeketov <problemkarlfree@gmail.com>
2022-10-27 20:46:58 +0300

    completed

---
+++ src/entities/fleet.py
@@ -0,0 +1,5 @@
+class Fleet():
+    
+    def __init__(self, id_val, name_val):
+        self.id = id_val
+        self.name = name_val
---
+++ src/entities/fleet_driver.py
@@ -0,0 +1,5 @@
+class FleetDriver():
+
+    def __init__(self, idOffice_val, idEmpl_val):
+        self.idOffice = idOffice_val
+        self.idEmpl = idEmpl_val
