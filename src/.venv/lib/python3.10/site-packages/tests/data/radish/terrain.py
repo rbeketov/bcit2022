29176b97278759dcdff290d168c385382bc81937
rbeketov <problemkarlfree@gmail.com>
2022-12-19 14:33:16 +0300

    report

--- src/.venv/lib/python3.10/site-packages/tests/data/radish/terrain.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-from radish import before
-
-
-@before.all
-def terrain_before_all(features, marker):
-    pass
--- src/.venv/lib/python3.10/site-packages/tests/exploratory/custom_types/example/model.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-class Hero(object):
-    def __init__(self, forename, surname, hero):
-        self.forename = forename
-        self.surname = surname
-        self.heroname = hero
